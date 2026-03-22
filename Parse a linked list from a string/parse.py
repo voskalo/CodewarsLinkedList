e55a1361d14a0bed9453bd047ed9fed394fc949c
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-22 15:30:14 +0200

    2nd task result + screenshot of tests passed

--- Parse a linked list from a string/parse.py
+++ Parse a linked list from a string/parse.py
@@ -7,4 +7,22 @@ class Node:
 
 def linked_list_from_string(list_repr: str) -> Node | None:
     '''test funct'''
-    return None
+
+    if list_repr is None:
+        return None
+
+    parts = list_repr.split(' -> ')
+    first = None
+    cur = None
+
+    for el in parts[:-1]:
+        node = Node(int(el))
+        if not first:
+            first = node
+            cur = node
+
+        else:
+            cur.next = node
+            cur = node
+
+    return first
