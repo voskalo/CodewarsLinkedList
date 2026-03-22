ce2b990c2dbb63967f1703cf51204084fd7a4b0c
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-22 17:30:55 +0200

    8th task preload and preparation

---
+++ middle tasks/Linked Lists - Remove Duplicates/remove_duplicates.py
@@ -0,0 +1,11 @@
+'''remove duplicates'''
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    '''function'''
+
+    ...
