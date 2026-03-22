eac34fdbdbaee67a61671281f1f74b48152624e7
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-22 15:21:55 +0200

    2nd task preload and preparation

---
+++ Parse a linked list from a string/parse.py
@@ -0,0 +1,10 @@
+'''Parse a linked list from a string'''
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(list_repr: str) -> Node | None:
+    '''test funct'''
+    return None
