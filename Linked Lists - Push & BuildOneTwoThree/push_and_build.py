047414aa1323f389928411a69e2a40d3f41f1456
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-22 15:32:26 +0200

    preload and preparation of task 3

---
+++ Linked Lists - Push & BuildOneTwoThree/push_and_build.py
@@ -0,0 +1,14 @@
+'''Linked Lists - Push & BuildOneTwoThree'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def push(head, data):
+    # Your code goes here.
+    return Node(None)
+
+def build_one_two_three():
+    # Your code goes here.
+    return Node(None)
