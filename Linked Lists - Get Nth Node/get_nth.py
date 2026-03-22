25e60537e8d468ca4b1fc880cfdf0cf74efbda7a
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-22 16:12:56 +0200

    4th task preparation and preload

---
+++ Linked Lists - Get Nth Node/get_nth.py
@@ -0,0 +1,11 @@
+'''Linked Lists - Get Nth Node'''
+
+class Node(object):
+    """Node class for reference"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def get_nth(node, index):
+    ...
+    # Your code goes here.
