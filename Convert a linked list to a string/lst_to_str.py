2ce78c45aaf6d0bf3eb088034adc48ff58941b69
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-22 14:55:50 +0200

    Preload and preparing to work

---
+++ Convert a linked list to a string/lst_to_str.py
@@ -0,0 +1,11 @@
+'''Convert a linked list to a string'''
+
+class Node():
+    def __init__(self, data, next = None):
+        '''init'''
+        self.data = data
+        self.next = next
+
+    def stringify(self):
+        '''task func'''
+        ...
