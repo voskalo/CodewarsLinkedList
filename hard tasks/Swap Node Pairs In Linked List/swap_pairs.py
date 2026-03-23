791bdc83008e37526d5b5ba547d0b4328ad0d041
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-23 10:56:23 +0200

    first try, added basic logic, don't work properly

--- hard tasks/Swap Node Pairs In Linked List/swap_pairs.py
+++ hard tasks/Swap Node Pairs In Linked List/swap_pairs.py
@@ -6,6 +6,24 @@ class Node:
         '''init'''
         self.next = next
 
-def swap_pairs(head):
+def swap_pairs(head: 'Node'):
     '''funct'''
-    return head
+
+    if not head or not head.next:
+        return head
+
+    node = Node()
+    node.next = head
+    prev = node
+
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = first.next
+
+        first.next = second.next
+        second.next = first
+        prev.next = second
+
+        prev = first
+
+    return node
