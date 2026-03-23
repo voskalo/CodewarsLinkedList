1677b7d8563ef0cb1e26c6736db01ddf7a0985fa
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-23 11:00:25 +0200

    10th task preparation and preloaad

---
+++ hard tasks/Linked Lists - Alternating Split/alt_split.py
@@ -0,0 +1,18 @@
+'''Linked Lists - Alternating Split'''
+
+class Node(object):
+    '''smth'''
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    '''smth'''
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+
+def alternating_split(head):
+    '''smth'''
+    ...
