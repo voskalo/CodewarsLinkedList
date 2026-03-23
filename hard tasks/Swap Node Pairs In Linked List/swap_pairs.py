b52026e9daed9280147052766a63aa6f537dc299
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-23 10:44:58 +0200

    9th task preload and preparation

---
+++ hard tasks/Swap Node Pairs In Linked List/swap_pairs.py
@@ -0,0 +1,11 @@
+'''Swap Node Pairs In Linked List'''
+
+class Node:
+    '''smth'''
+    def __init__(self, next=None):
+        '''init'''
+        self.next = next
+
+def swap_pairs(head):
+    '''funct'''
+    return head
