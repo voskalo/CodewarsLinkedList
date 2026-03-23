603c90f38202d796e5ca9d43dd683eef955d1493
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2026-03-23 11:31:47 +0200

    11th task preload and preparation

---
+++ hard tasks/Can you get the loop ?/loop.py
@@ -0,0 +1,12 @@
+'''Can you get the loop ?'''
+
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+def loop_size(node):
+    '''smth'''
+    pass
